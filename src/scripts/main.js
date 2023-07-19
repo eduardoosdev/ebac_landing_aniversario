75ce4c81c1e8921bb5499413360d07966340969a
eduardoosdev <60430101+eduardoosdev@users.noreply.github.com>
2023-07-19 17:51:48 -0300

    Criando a contagem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-console.log("Olá miau");
+AOS.init();
+
+const dataDoEvento = new Date("Jul 19 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOevento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOevento}s`
+
+    if(distanciaAteOEvento < 0) {
+         clearInterval(contaAsHoras);
+         document.getElementById('contador').innerHTML = 'EVENTO EXPIRADO';
+    }
+}, 1000);
